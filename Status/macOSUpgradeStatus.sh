612fa5f7d02df76ef28902bfdaa73d373a67899d
pwalker1485 <pwalker1485@icloud.com>
2020-08-03 22:25:33 +0100

    minor changes for 10.15 upgrades

--- Status/macOSUpgradeStatus.sh
+++ Status/macOSUpgradeStatus.sh
@@ -4,86 +4,79 @@
 #                   macOS Upgrade Pre Check Status                     #
 ################## Written by Phil Walker Sept 2019 ####################
 ########################################################################
+# Modified June 2020
 
 ########################################################################
 #                            Variables                                 #
 ########################################################################
 
-#Get the logged in user
-loggedInUser=$(python -c 'from SystemConfiguration import SCDynamicStoreCopyConsoleUser; import sys; username = (SCDynamicStoreCopyConsoleUser(None, None, None) or [None])[0]; username = [username,""][username in [u"loginwindow", None, u""]]; sys.stdout.write(username + "\n");')
-#Check the logged in user has a local account (for 10.12 MacBooks only)
-mobileAccount=$(dscl . read /Users/${loggedInUser} OriginalNodeName 2>/dev/null)
-#Mac model
-macModel=$(sysctl -n hw.model)
-#Path to NoMAD Login AD bundle
+# Get the logged in user
+loggedInUser=$(stat -f %Su /dev/console)
+# Check the logged in user has a local account (for 10.12 MacBooks only)
+mobileAccount=$(dscl . -read /Users/"$loggedInUser" OriginalNodeName 2>/dev/null)
+# Path to NoMAD Login AD bundle
 noLoADBundle="/Library/Security/SecurityAgentPlugins/NoMADLoginAD.bundle"
-#Installer location
-macOSInstaller="/Applications/Install macOS Mojave.app"
-#Required disk space
+# Installer location
+macOSInstaller="/Applications/Install macOS Catalina.app"
+# Required disk space
 requiredSpace="15"
-#OS version
-osMinor=$(/usr/bin/sw_vers -productVersion | awk -F. {'print $2'})
+# OS version
+osShort=$(/usr/bin/sw_vers -productVersion | awk -F. '{print $2}')
 
 ########################################################################
 #                         Script starts here                           #
 ########################################################################
 
-#Get available disk space
-if [[ "$osMinor" -eq "12" ]]; then
+# Get available disk space
+if [[ "$osShort" -eq "12" ]]; then
 	freeSpace=$(/usr/sbin/diskutil info / | grep "Available Space" | awk '{print $4}')
 else
   freeSpace=$(/usr/sbin/diskutil info / | grep "Free Space" | awk '{print $4}')
 fi
 
-if [ -z ${freeSpace} ]; then
+if [ -z "$freeSpace" ]; then
   freeSpace="5"
 fi
 
-#Confirm there is enough disk space for the upgrade
+# Confirm there is enough disk space for the upgrade
 if [[ ${freeSpace%.*} -ge ${requiredSpace} ]]; then
 	spaceStatus="OK"
 fi
 
-#Confirm the installer is available
+# Confirm the installer is available
 if [[ -d "$macOSInstaller" ]]; then
-  mojaveInstaller="Found"
+	catalinaInstaller="Found"
 else
-	mojaveInstaller="Not Found"
+	catalinaInstaller="Not Found"
 fi
 
-#Get account status of logged in user (Local or Mobile)
+# Get account status of logged in user (Local or Mobile)
 if [[ "$mobileAccount" == "" ]]; then
 	accountStatus="Local"
 else
 	accountStatus="Mobile"
 fi
 
-#Upgrade Status
-if [[ "$osMinor" -eq "12" ]] && [[ "$macModel" =~ "MacBook" ]]; then
+# Upgrade Status
+if [[ "$osShort" -eq "14" ]]; then
 	if [[ -d "$noLoADBundle" ]] && [[ "$accountStatus" == "Local" ]]; then
-		if [[ "$spaceStatus" == "OK" ]] && [[ "$mojaveInstaller" == "Found" ]]; then
-      echo "<result>Upgrade Ready</result>"
-    else
-      echo "<result>Disk space:${freeSpace}GB | Installer:${mojaveInstaller} | Account status:${accountStatus}</result>"
+		if [[ "$spaceStatus" == "OK" ]] && [[ "$catalinaInstaller" == "Found" ]]; then
+      		echo "<result>Upgrade Ready</result>"
+    	else
+      		echo "<result>Disk space:${freeSpace}GB | Installer:${catalinaInstaller} | Account status:${accountStatus}</result>"
 		fi
 	else
-  	echo "<result>Disk space:${freeSpace}GB | Installer:${mojaveInstaller} | Account status:${accountStatus}</result>"
-	fi
-elif [[ "$osMinor" -eq "12" ]] && [[ ! "$macModel" =~ "MacBook" ]]; then
-	if [[ "$spaceStatus" == "OK" ]] && [[ "$mojaveInstaller" == "Found" ]]; then
-		echo "<result>Upgrade Ready</result>"
-	else
-		echo "<result>Disk space:${freeSpace}GB | Installer:${mojaveInstaller}</result>"
+  		echo "<result>Disk space:${freeSpace}GB | Installer:${catalinaInstaller} | Account status:${accountStatus}</result>"
 	fi
 fi
 
-if [[ "$osMinor" -ge "14" ]]; then
+if [[ "$osShort" -ge "15" ]]; then
   echo "<result>Not Required</result>"
-elif [[ "$osMinor" -le "11" ]] || [[ "$osMinor" -eq "13" ]]; then
-  if [[ "$spaceStatus" == "OK" ]] && [[ "$mojaveInstaller" == "Found" ]]; then
+elif [[ "$osShort" -le "11" ]] || [[ "$osShort" -eq "13" ]]; then
+  if [[ "$spaceStatus" == "OK" ]] && [[ "$catalinaInstaller" == "Found" ]]; then
     echo "<result>Upgrade Ready</result>"
   else
-    echo "<result>Disk space:${freeSpace}GB | Installer:${mojaveInstaller}</result>"
+    echo "<result>Disk space:${freeSpace}GB | Installer:${catalinaInstaller}</result>"
   fi
 fi
 
