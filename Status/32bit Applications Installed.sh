a7522cd88521c9e3afed359c12378e372e89ef00
pwalker1485 <pwalker1485@icloud.com>
2020-10-07 15:01:23 +0100

    amended for Big Sur and to exclude content on external disks

--- Status/32bit Applications Installed.sh
+++ Status/32bit Applications Installed.sh
@@ -8,13 +8,14 @@
 #                            Variables                                 #
 ########################################################################
 
-# OS version - short
-osShort=$(sw_vers -productVersion | awk -F. '{print $2}')
+# OS version - amended to account for Big Sur
+osShort=$(SYSTEM_VERSION_COMPAT=1 sw_vers -productVersion | awk -F. '{print $2}')
 # Check for 32-bit Applications
 # If running macOS Mojave or previous, exclude the system app InkServer.app and the system framework QuickLook which includes quicklookd32.app (both are 32-bit)
+# Added additional exclusion for external disks for 10.14 as so many users have old crap on external disks
 if [[ "$osShort" -le "14" ]]; then
     thirtyTwoBitApps=$(/usr/bin/mdfind "kMDItemExecutableArchitectures == 'i386' && kMDItemExecutableArchitectures != 'x86_64' && \
-kMDItemKind == 'Application'" | grep -v "/System/Library/Input Methods/InkServer.app" | grep -v "/System/Library/Frameworks/QuickLook.framework/Versions/A/Resources/quicklookd32.app")
+kMDItemKind == 'Application'" | grep -v "/System/Library/Input Methods/InkServer.app" | grep -v "/System/Library/Frameworks/QuickLook.framework/Versions/A/Resources/quicklookd32.app" | grep -v "/Volumes")
 else
     thirtyTwoBitApps=$(/usr/bin/mdfind "kMDItemExecutableArchitectures == 'i386' && kMDItemExecutableArchitectures != 'x86_64' && kMDItemKind == 'Application'")
 fi
@@ -28,5 +29,4 @@ if [[ "${thirtyTwoBitApps}" != "" ]]; then
 else
     echo "<result>No</result>"
 fi
-
 exit 0
