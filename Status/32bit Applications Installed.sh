3b6f677731aaf9378a5583cbdd3652adf3558e88
pwalker1485 <pwalker1485@icloud.com>
2020-10-30 16:36:59 +0000

    switched to zsh and is-at-least for OS version check

--- Status/32bit Applications Installed.sh
+++ Status/32bit Applications Installed.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/zsh
 
 ########################################################################
 #          32-bit Applications Installed - Extention Attribute         #
@@ -8,14 +8,19 @@
 #                            Variables                                 #
 ########################################################################
 
-# OS version - amended to account for Big Sur
-osShort=$(SYSTEM_VERSION_COMPAT=1 sw_vers -productVersion | awk -F. '{print $2}')
+# OS Version
+osVersion=$(sw_vers -productVersion)
+# macOS Catalina version number
+catalinaOS="10.15"
 # Check for 32-bit Applications
 # If running macOS Mojave or previous, exclude the system app InkServer.app and the system framework QuickLook which includes quicklookd32.app (both are 32-bit)
 # Added additional exclusion for external disks for 10.14 as so many users have old crap on external disks
-if [[ "$osShort" -le "14" ]]; then
+# Added additional exclusion for users OneDrive sync directory due to old crap having being uploaded to OneDrive
+autoload is-at-least
+if ! is-at-least "$catalinaOS" "$osVersion"; then
     thirtyTwoBitApps=$(/usr/bin/mdfind "kMDItemExecutableArchitectures == 'i386' && kMDItemExecutableArchitectures != 'x86_64' && \
-kMDItemKind == 'Application'" | grep -v "/System/Library/Input Methods/InkServer.app" | grep -v "/System/Library/Frameworks/QuickLook.framework/Versions/A/Resources/quicklookd32.app" | grep -v "/Volumes")
+kMDItemKind == 'Application'" | grep -v "/System/Library/Input Methods/InkServer.app" | grep -v "/System/Library/Frameworks/QuickLook.framework/Versions/A/Resources/quicklookd32.app" | \
+grep -v "/Volumes" | grep -v "OneDrive - Bauer Media Group")
 else
     thirtyTwoBitApps=$(/usr/bin/mdfind "kMDItemExecutableArchitectures == 'i386' && kMDItemExecutableArchitectures != 'x86_64' && kMDItemKind == 'Application'")
 fi
