3e095885908679bf61491342cd0414afae272112
pwalker1485 <pwalker1485@icloud.com>
2020-11-25 19:34:39 +0000

    initial upload

---
+++ Status/SoftwareUpdateScheduleStatus.sh
@@ -0,0 +1,29 @@
+#!/bin/zsh
+
+########################################################################
+#                Software Update Schedule Status - EA                  #
+########################################################################
+
+########################################################################
+#                            Variables                                 #
+########################################################################
+
+# OS Version
+osVersion=$(sw_vers -productVersion)
+# Minimum OS Version
+minOS="10.13"
+# Software Update 
+suStatus=$(softwareupdate --schedule)
+
+########################################################################
+#                         Script starts here                           #
+########################################################################
+
+# Load is-at-least
+autoload is-at-least
+# If running macOS 10.13 or later then report the status
+if is-at-least "$minOS" "$osVersion"; then
+    echo "<result>$suStatus</result>"
+else
+    echo "<result>OS Not Supported</result>"
+fi
